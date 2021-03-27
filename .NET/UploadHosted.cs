c3e3c303906e760cdf41409aa2222ed1176f8974
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-03-27 16:17:58 -0500

    Added C# Snippets

---
+++ .NET/UploadHosted.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace UploadLocal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_KEY = ""; // Your API Key
+            string DATASET_NAME = "your-dataset"; // Set Dataset Name (Found in Dataset URL)
+            string imageURL = "https://i.imgur.com/PEEvqPN.png";
+            imageURL = HttpUtility.UrlEncode(imageURL);
+
+            // Construct the URL
+            string uploadURL =
+                    "https://api.roboflow.com/dataset/" +
+                            DATASET_NAME + "/upload" +
+                            "?api_key=" + API_KEY +
+                            "&name=YOUR_IMAGE.jpg" +
+                            "&split=train" +
+                            "&image=" + imageURL;
+
+            // Service Point Config
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // Configure Http Request
+            WebRequest request = WebRequest.Create(uploadURL);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = 0;
+
+            // Get Response
+            string responseContent = null;
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader sr99 = new StreamReader(stream))
+                    {
+                        responseContent = sr99.ReadToEnd();
+                    }
+                }
+            }
+
+            Console.WriteLine(responseContent);
+
+        }
+    }
+}
---
+++ .NET/UploadLocal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace UploadLocal
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte[] imageArray = System.IO.File.ReadAllBytes(@"YOUR_IMAGE.jpg");
+            string encoded = Convert.ToBase64String(imageArray);
+            byte[] data = Encoding.ASCII.GetBytes(encoded);
+            string API_KEY = ""; // Your API Key
+            string DATASET_NAME = "your-dataset"; // Set Dataset Name (Found in Dataset URL)
+
+            // Construct the URL
+            string uploadURL =
+                    "https://api.roboflow.com/dataset/" +
+                            DATASET_NAME + "/upload" +
+                            "?api_key=" + API_KEY +
+                            "&name=YOUR_IMAGE.jpg" +
+                            "&split=train";
+
+            // Service Request Config
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // Configure Request
+            WebRequest request = WebRequest.Create(uploadURL);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            // Write Data
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            // Get Response
+            string responseContent = null;
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader sr99 = new StreamReader(stream))
+                    {
+                        responseContent = sr99.ReadToEnd();
+                    }
+                }
+            }
+
+            Console.WriteLine(responseContent);
+
+        }
+    }
+}
